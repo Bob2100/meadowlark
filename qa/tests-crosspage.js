9c988dfed222f03fca35e31107eedd46954342d4
Bob <bob_w@qq.com>
2022-01-27 10:56:00 +0800

    测试用例

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -7,5 +7,26 @@ suite('Cross-page Tests', function () {
   })
   test('requesting a group rate quote from the hood river page should populate the referrer field', function (done) {
     const referrer = 'http://localhost:3000/tours/hood-river'
+    browser.visit(referrer, function () {
+      browser.clickLink('.requestGroupRate', function () {
+        assert(browser.field('referrer').value === referrer)
+        done()
+      })
+    })
+  })
+  test('requesting a group rate quote from the oregon coast page should populate the referrer field', function (done) {
+    const referrer = 'http://localhost:3000/tours/oregon-coast'
+    browser.visit(referrer, function () {
+      browser.clickLink('.requestGroupRate', function () {
+        assert(browser.field('referrer').value === referrer)
+        done()
+      })
+    })
+  })
+  test('visiting the "request group rate" page directly should result in an empty referrer field', function (done) {
+    browser.visit('http://localhost:3000/tours/equest-group-rate', function () {
+      assert(browser.field('referrer').value === '')
+      done()
+    })
   })
 })
